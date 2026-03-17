4ad377c79e8e7d5ff59c9fce563f75bebc4f49de
Grannik <81516234+Grannik@users.noreply.github.com>
2026-03-17 21:01:26 +0100

    Add files via upload

---
+++ content_help.h
@@ -0,0 +1,44 @@
+#ifndef PSEUDOGRAPHIC_HELP_H
+#define PSEUDOGRAPHIC_HELP_H
+#define HELP_TOTAL_CONTENT 36
+
+static const char *help_content[HELP_TOTAL_CONTENT] = {
+    " module 0 | Reference information. Module - usage.",
+    " module 1 | Clock (style 1)",
+    " module 2 | Clock (3-line pseudographic font)",
+    " module 3 | Clock (5-line pseudographic font)",
+    " module 4 | Clock (7-line pseudographic font)",
+    " module 5 | Basic ANSI Text Attributes and 16-Color Palette List",
+    " module 6 | 256-Color ANSI Palette List",
+    " module 7 | marquee",
+    " module 8 | empty",
+    " module 9 | empty",
+    " module a | empty",
+    " module b | empty",
+    " module c | empty",
+    " module d | empty",
+    " module e | empty",
+    " module f | empty",
+    " module g | empty",
+    " module h | empty",
+    " module i | empty",
+    " module j | empty",
+    " module k | empty",
+    " module l | empty",
+    " module m | empty",
+    " module n | empty",
+    " module o | empty",
+    " module p | empty",
+    " module q | empty",
+    " module r | empty",
+    " module s | empty",
+    " module t | empty",
+    " module u | empty",
+    " module v | empty",
+    " module w | empty",
+    " module x | empty",
+    " module y | empty",
+    " module z | empty",
+};
+
+#endif
