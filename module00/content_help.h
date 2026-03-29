26fecd5377904aed9de804844ccf718d03ad4e14
Grannik <81516234+Grannik@users.noreply.github.com>
2026-03-30 01:33:17 +0200

    Create content_help.h

---
+++ module00/content_help.h
@@ -0,0 +1,44 @@
+#ifndef PSEUDOGRAPHIC_HELP_H
+#define PSEUDOGRAPHIC_HELP_H
+#define HELP_TOTAL_CONTENT 36
+
+static const char *help_content[HELP_TOTAL_CONTENT][2] = {
+    { "module 0 | Reference information. Module - usage.", "Reference information. Module - usage\n Navigation modules: [0 - 9 A - Z] keys\n Small-Screen Mode\n Use Up/Down arrows to scroll lines" },
+    { "module 1 | Clock (style 1)", "Clock (style 1)\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module 2 | Clock (3-line pseudographic font)", "Clock (3-line pseudographic font)\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module 3 | Clock (5-line pseudographic font)", "Clock (5-line pseudographic font)\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module 4 | Clock (7-line pseudographic font)", "Clock (7-line pseudographic font)\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module 5 | Clock (9-line pseudographic font)", "Clock (9-line pseudographic font)\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module 6 | Basic ANSI Text Attributes and 16-Color Palette List", "Basic ANSI Text Attributes and 16-Color Palette List\n Navigation modules: [0 - 9 A - Z] keys\n Use Up/Down arrows to scroll lines" },
+    { "module 7 | 256-Color ANSI Palette List \\033[38;5;*m", "256-Color ANSI Palette List \\033[38;5;*m\n Navigation modules: [0 - 9 A - Z] keys\n Use Up/Down arrows to scroll lines" },
+    { "module 8 | 256-Color ANSI Palette List \\033[48;5;*m", "256-Color ANSI Palette List \\033[48;5;*m\n Navigation modules: [0 - 9 A - Z] keys\n Use Up/Down arrows to scroll lines" },
+    { "module 9 | C Format Specifiers List", "C Format Specifiers List\n Navigation modules: [0 - 9 A - Z] keys\n Use Up/Down arrows to scroll lines" },
+    { "module A | ABC", "ABC\n Navigation modules: [0 - 9 A - Z] keys\n Use Up/Down arrows to scroll lines" },
+    { "module B | FRAME", "FRAME\n Navigation modules: [0 - 9 A - Z] keys\n Small-Screen Mode\n Block View: Left/Right Keys" },
+    { "module C | Complex of modules file analyzer", "Complex of modules file analyzer\n Navigation modules: [0 - 9 A - Z] keys\n Use Up/Down arrows to scroll FILES & DIRECTORIES\n Enter FILE ANALYSIS RESULTS\n Shift + z Close" },
+    { "module D | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module E | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module F | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module G | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module H | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module I | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module J | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module K | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module L | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module M | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module N | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module O | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module P | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module Q | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module R | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module S | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module T | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module U | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module V | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module W | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module X | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module Y | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" },
+    { "module Z | empty", "Empty\n Navigation modules: [0 - 9 A - Z] keys" }
+};
+
+#endif
