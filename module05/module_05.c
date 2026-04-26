53b0114e8288eb7de23c8640a4c54bfba441c8e1
Grannik <81516234+Grannik@users.noreply.github.com>
2026-04-26 20:06:26 +0200

    Create module_05.c

---
+++ module05/module_05.c
@@ -0,0 +1,45 @@
+#include "common.h"
+#include <stdio.h>
+#include <unistd.h>
+#include "libtermcolor/libtermcolor.h"
+#include "libtermanimation/libtermanimation.h"
+
+int handle_common_input(char input_char, int *current_module);
+int module_05_draw(void);
+int module_05_update(void);
+
+int module_05_draw(void) {
+    werase(stdscr);
+    generic_frame_ncurses(stdscr, 1, 1, 84, termheight, 0, 2, -1, -1, 1, 0, 4, "GRANNIK | COMPLEX SOFTWARE ECOSYSTEM");
+    module_strip_ncurses(stdscr, 2, 2, 0, 2, -1, 7, '5', "Clock (9-line pseudographic font)");
+    int maxy, maxx;
+    getmaxyx(stdscr, maxy, maxx);
+    generic_abc_9_ncurses(stdscr, (maxx / 2) - 4, (maxy / 2) - 4, 0, 8, -1, ":");
+    ClockData cd;
+    clock_update(&cd);
+    int y = (maxy / 2) - 4;
+    int hour_x = (maxx / 2) - 25;
+    int min_x  = (maxx / 2) + 7;
+    var_abc_9_ncurses(stdscr, y, 0, 5, -1,
+        hour_x, cd.hour_str,
+        min_x,  cd.min_str,
+        -1);
+    wrefresh(stdscr);
+    return 5;
+}
+
+int module_05_update(void) {
+    int maxy, maxx;
+    getmaxyx(stdscr, maxy, maxx);
+    int y = (maxy / 2) - 4;
+    int hour_x = (maxx / 2) - 25;
+    int min_x  = (maxx / 2) + 7;
+    ClockData cd;
+    clock_update(&cd);
+    var_abc_9_ncurses(stdscr, y, 0, 5, -1,
+        hour_x, cd.hour_str,
+        min_x,  cd.min_str,
+        -1);
+    wrefresh(stdscr);
+    return 5;
+}
