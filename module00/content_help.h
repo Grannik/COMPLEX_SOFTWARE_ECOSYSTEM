c4b8478b6a946f0508ec4609150fee6f2feab5d6
Grannik <81516234+Grannik@users.noreply.github.com>
2026-04-26 19:19:09 +0200

    Create content_help.h

---
+++ module00/content_help.h
@@ -0,0 +1,215 @@
+#ifndef PSEUDOGRAPHIC_HELP_H
+#define PSEUDOGRAPHIC_HELP_H
+#define HELP_TOTAL_CONTENT 36
+
+static const char *help_content[HELP_TOTAL_CONTENT][17] = {
+     {"module_00.c | module 0 | Reference information. Module - usage.",
+      "* Reference information. Module - usage *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "Small-Screen Mode","Use Up/Down arrows to scroll lines"
+      "","","","","","","","","","","","",""},
+
+     {"module_01.c | module 1 | Clock (style 1)",
+      "* Clock (style 1) *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_02.c | module 2 | Clock (3-line pseudographic font)",
+      "* Clock (3-line pseudographic font) *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_03.c | module 3 | Clock (5-line pseudographic font)",
+      "* Clock (5-line pseudographic font) *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_04.c | module 4 | Clock (7-line pseudographic font)",
+      "* Clock (7-line pseudographic font) *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_05.c | module 5 | Clock (9-line pseudographic font)",
+      "* Clock (9-line pseudographic font) *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_06.c | module 6 | ANSI Text Attributes and 16-Color Palette List",
+      "* Basic ANSI Text Attributes and 16-Color Palette List *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "Use Up/Down arrows to scroll lines",
+      "","","","","","","","","","","","",""},
+
+     {"module_07.c | module 7 | 256-Color ANSI Palette List \\033[38;5;*m",
+      "* 256-Color ANSI Palette List \\033[38;5;m *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "Use Up/Down arrows to scroll lines",
+      "","","","","","","","","","","","",""},
+
+     {"module_08.c | module 8 | 256-Color ANSI Palette List \\033[48;5;*m",
+      "* 256-Color ANSI Palette List \\033[48;5;*m *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "Use Up/Down arrows to scroll lines",
+      "","","","","","","","","","","","",""},
+
+     {"module_09.c | module 9 | C Format Specifiers List",
+      "* C Format Specifiers List *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "Use Up/Down arrows to scroll lines",
+      "","","","","","","","","","","","",""},
+
+     {"module_10.c | module A | ABC",
+      "* ABC *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "Use Up/Down arrows to scroll lines",
+      "","","","","","","","","","","","",""},
+
+     {"module_11.c | module B | FRAME",
+      "* FRAME *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "Small-Screen Mode",
+      "Block View: Left/Right Keys",
+      "","","","","","","","","","","",""},
+
+     {"module_12.c | module C | Module file analyzer",
+      "* Module file analyzer *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "Use Up/Down arrows to scroll FILES & DIRECTORIES",
+      "Enter Show content being viewed",
+      "Z Close content being viewed",
+      "","","","","","","","","","",""},
+
+     {"module_13.c | module D | Module text viewer",
+      "* Module text viewer *",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "Use Up/Down arrows to scroll FILES & DIRECTORIES",
+      "Enter Show content being viewed",
+      "Use Up/Down/Left/Right arrows to scroll lines",
+      "Z Close content being viewed",
+      "","","","","","","","","",""},
+
+     {"module_14.c | module E | Music pcm player module",
+      "* Music pcm player module *",
+      "Switching by module: [0 - 9 A - Z] keys",
+      "↑↓ Move through the list",
+      "← Go back through history",
+      "→ Jump forward in history",
+      "Enter Enter directory or play .pcm",
+      "[space] play directory as playlist",
+      "- seek backward",
+      "= seek forward",
+      "[ scroll file path left",
+      "] scroll file path right",
+      "; loop playback",
+      ", pause playback",
+      ". stop playback",
+      "/ play previous file",
+      "\\ play next file"},
+
+     {"module_15.c | module F | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_16.c | module G | empty",
+      "Empty",
+      " Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_17.c | module H | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_18.c | module I | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_19.c | module J | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_20.c | module K | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_21.c | module L | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_22.c | module M | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_23.c | module N | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_24.c | module O | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_25.c | module P | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_26.c | module Q | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_27.c | module R | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_28.c | module S | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_29.c | module T | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_30.c | module U | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_31.c | module V | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_32.c | module W | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_32.c | module X | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_34.c | module Y | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""},
+
+     {"module_35.c | module Z | empty",
+      "Empty",
+      "Navigation modules: [0 - 9 A - Z] keys",
+      "","","","","","","","","","","","","",""}
+};
+
+#endif
